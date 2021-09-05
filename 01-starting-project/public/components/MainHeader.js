3f3c5defc353080a72795ebd989ca00edffac3e2
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-05 10:24:34 +0300

    structured the files

---
+++ 01-starting-project/public/components/MainHeader.js
@@ -0,0 +1,16 @@
+const MainHeader = () => {
+	return (
+		<div>
+			<ul>
+				<li>
+					<a href="/welcome">Welcome</a>
+				</li>
+				<li>
+					<a href="/products">Products</a>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default MainHeader;
--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,9 +1,20 @@
+import { Route } from 'react-router-dom';
+import MainHeader from '../public/components/MainHeader';
+import Products from '../public/pages/Products';
+import Welcome from '../public/pages/Welcome';
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+	return (
+		<div>
+			<MainHeader />
+			<Route path="/welcome">
+				<Welcome />
+			</Route>
+			<Route path="/products">
+				<Products />
+			</Route>
+		</div>
+	);
 }
 
 export default App;
