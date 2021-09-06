74171d8124a3faa41152064036985c00d505c5d1
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-09-06 09:49:03 +0300

    added layout component

--- 09-time-to-practice-starting-code/src/App.js
+++ 09-time-to-practice-starting-code/src/App.js
@@ -1,21 +1,24 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Layout from './components/layout/Layout';
 import { NewQuote, QuoteDetail, Quotes } from './pages';
 function App() {
 	return (
-		<Switch>
-			<Route path="/" exact>
-				<Redirect to="/quotes" />
-			</Route>
-			<Route path="/quotes" exact>
-				<Quotes />
-			</Route>
-			<Route path="/quotes/new" exact>
-				<NewQuote />
-			</Route>
-			<Route path="/quotes/:quoteId">
-				<QuoteDetail />
-			</Route>
-		</Switch>
+		<Layout>
+			<Switch>
+				<Route path="/" exact>
+					<Redirect to="/quotes" />
+				</Route>
+				<Route path="/quotes" exact>
+					<Quotes />
+				</Route>
+				<Route path="/new" exact>
+					<NewQuote />
+				</Route>
+				<Route path="/quotes/:quoteId">
+					<QuoteDetail />
+				</Route>
+			</Switch>
+		</Layout>
 	);
 }
 
